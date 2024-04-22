a41d2528759a40e7f0389bab8951998dba6c4671
DeadEyeDie <113912677+DeadEyeDie@users.noreply.github.com>
2024-04-22 15:28:40 +0200

    fungerende fil

---
+++ src/test/java/com/example/oblig3data1700maven/Oblig3Data1700mavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3data1700maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3Data1700mavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
